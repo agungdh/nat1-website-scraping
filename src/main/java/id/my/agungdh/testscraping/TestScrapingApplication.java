50e37769dcf94a89d1e1c52da5c77f7eb4a7a39a
AgungDH <agungdh@live.com>
2026-07-10 17:09:47 +0700

    init

---
+++ src/main/java/id/my/agungdh/testscraping/TestScrapingApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.testscraping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestScrapingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestScrapingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/agungdh/testscraping/TestScrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.testscraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestScrapingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
